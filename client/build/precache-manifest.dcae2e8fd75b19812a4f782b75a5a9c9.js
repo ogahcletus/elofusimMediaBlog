236ad042021f7c95c911e16f5abce97437ca19fd
Cletus Ogah <ogahcletus@gmail.com>
2022-10-06 10:45:30 +0100

    added build files

---
+++ client/build/precache-manifest.dcae2e8fd75b19812a4f782b75a5a9c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e67e523865222e2006e",
+    "url": "/static/css/main.726d0ede.chunk.css"
+  },
+  {
+    "revision": "8e67e523865222e2006e",
+    "url": "/static/js/main.35a34e38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74cabe19c2d3b3d35b0c",
+    "url": "/static/js/2.021c9d4c.chunk.js"
+  },
+  {
+    "revision": "8fa64a27ab8d84a70ca8b64ad58355ae",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -10,11 +10,13 @@ const multer = require("multer");
 const path = require("path");
 const colors = require('colors')
 const cors = require('cors')
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
 app.use(cors())
 app.use("/images", express.static(path.join(__dirname, "/images")));
+
 
 mongoose
   .connect(process.env.MONGO_URI, {
