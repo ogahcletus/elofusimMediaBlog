f9778b24278befaaf1cd8322f9fdef52758021a8
Cletus Ogah <ogahcletus@gmail.com>
2022-10-06 11:25:49 +0100

    modify server

--- server.js
+++ server.js
@@ -49,7 +49,11 @@ app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  });
 
 }
 
